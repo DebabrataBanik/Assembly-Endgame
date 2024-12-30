467d496a54fd1c90419b256b99a3c4131bce6b45
DebabrataBanik <debabratabanik02@gmail.com>
2024-12-30 23:56:21 +0530

    base layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,52 @@
+import { languages } from "./languages"
+import { useState } from 'react'
+
 function App() {
+
+  const [word, setWord] = useState('react')
+
+  const alphabets = "abcdefghijklmnopqrstuvwxyz"
+
   return (
     <main className="container">
-      <h1>Assembly Endgame</h1>
-      <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
-      <section className="game-result">
+      <header>
+        <h1>Assembly Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
 
+      <section className="game-result">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
       </section>
+
       <section className="languages__container">
         <ul className="languages">
-          <li><button>HTML</button></li>
-          <li><button>CSS</button></li>
-          <li><button>Javascript</button></li>
-          <li><button>React</button></li>
-          <li><button>Typescript</button></li>
-          <li><button>Node.js</button></li>
-          <li><button>Python</button></li>
-          <li><button>Ruby</button></li>
-          <li><button>Assembly</button></li>
+          {
+            languages.map(language => (
+              <li key={language.name}><button style={{
+                backgroundColor: language.backgroundColor,
+                color: language.color
+              }}>{language.name}</button></li>
+            ))
+          }
         </ul>
       </section>
 
-      <section className="guesses__container">
+      <section className="word">
+        {
+          Array.from(word).map((char, index) => (
+            <span className="char" key={index}>{char}</span>
+          ))
+        }
 
       </section>
 
-      <section className="keyboard__container">
-
+      <section className="keyboard">
+        {
+          alphabets.split('').map(char => (
+            <button className="key" key={char}>{char}</button>
+          ))
+        }
       </section>
 
       <button className="newgame">
